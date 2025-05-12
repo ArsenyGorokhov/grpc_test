abdb6b25ad629d253519c0f97120b31ebafc7673
Arseny Gorokhov <Arseny.Gorokhoff@yandex.ru>
2025-05-12 09:18:18 +0500

    main

--- chat-server/cmd/main.go
+++ chat-server/cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	desc "github.com/ArsenyGorokhov/grpc_test/chat-server/pkg/chat"
+)
+
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedChatServer
+}
+
 func main() {
 
 }
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module grpc_test
+module github.com/ArsenyGorokhov/grpc_test
 
 go 1.24.3
 

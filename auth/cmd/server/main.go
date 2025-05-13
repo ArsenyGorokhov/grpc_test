9148558461f6b78a9a08b94762031c4b1ba1f277
Arseny Gorokhov <70861765+ArsenyGorokhov@users.noreply.github.com>
2025-05-13 12:45:15 +0500

    ww

--- auth/cmd/server/main.go
+++ auth/cmd/server/main.go
@@ -44,14 +44,12 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-
 	reflection.Register(s)
-
 	desc.RegisterUserServer(s, &server{})
 
 	log.Printf("server listening at %v", lis.Addr())
 
-	if err := s.Serve(lis); err != nil {
+	if err = s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }

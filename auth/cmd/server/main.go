21bcf2445619262ddc3230a7c062cf1ffca283dc
Arseny Gorokhov <70861765+ArsenyGorokhov@users.noreply.github.com>
2025-05-14 23:58:55 +0500

    some handlers added to a auth server

--- auth/cmd/server/main.go
+++ auth/cmd/server/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/brianvoe/gofakeit"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -20,7 +21,7 @@ type server struct {
 }
 
 func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
-	fmt.Println("Get Handler been used")
+	fmt.Println("Get is here")
 	password := gofakeit.Password(true, true, true, true, false, 12)
 	return &desc.GetResponse{
 		User: &desc.UserInfo{
@@ -37,8 +38,35 @@ func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetRespon
 	}, nil
 }
 
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	fmt.Println("Update is here")
+	if req == nil {
+		return nil, fmt.Errorf("request is nil")
+	}
+	log.Printf("Information updated: id=%d, email=%s, name=%s, role=%s",
+		req.GetId(),
+		req.GetEmail(),
+		req.GetName(),
+		req.GetRole())
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	fmt.Println("Create is here")
+	log.Printf("Created %v", req.Info)
+	return &desc.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	fmt.Println("Delete is here")
+	log.Printf("User #%d to be deleleted", req.Id)
+	return &emptypb.Empty{}, nil
+}
+
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf("%:d", grpcPort))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
